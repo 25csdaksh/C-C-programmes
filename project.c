f10fdf0ff4e83db2e73c2f969fc49acd0f4bdfee
Daksh Soni <25cs102@charusat.edu.in>
2026-03-02 13:03:57 +0530

    Refactor rectangle area calculation with validation
    Updated variable names and added input validation for length and breadth.

--- project.c
+++ project.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
-int main()  
-{
-    int l, b;
-    printf("\nEnter the value of l =");
-    scanf("%d", &l);
 
-    printf("\nEnter the value of b =");
-    scanf("%d", &b);
+int main() {
+    int length, breadth;
 
-    printf("The area of rectangle is : %d", l * b);
+    printf("Enter the length of the rectangle: ");
+    if (scanf("%d", &length) != 1 || length <= 0) {
+        printf("Invalid input! Length must be a positive integer.\n");
+        return 1;
+    }
+
+    printf("Enter the breadth of the rectangle: ");
+    if (scanf("%d", &breadth) != 1 || breadth <= 0) {
+        printf("Invalid input! Breadth must be a positive integer.\n");
+        return 1;
+    }
+
+    int area = length * breadth;
+    printf("Rectangle dimensions: %d x %d\n", length, breadth);
+    printf("The area of the rectangle is: %d\n", area);
+
     return 0;
 }
